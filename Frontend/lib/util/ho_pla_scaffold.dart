22742e9cc0588568b43a6ad9f9efdb5e0984252a
Clemens <mail@clemens.jann.xyz>
2023-10-17 10:37:46 +0200

    Feat: add custom scaffold

---
+++ Frontend/lib/util/ho_pla_scaffold.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+/// A basic scaffold. It only has a navigation button if a page can be popped.
+/// Allows custom title and body.
+class HoPlaScaffold extends StatelessWidget {
+  final String title;
+  final Widget body;
+
+  const HoPlaScaffold(this.title, this.body, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+          centerTitle: true,
+          // Only display the BackButton if the user can navigate back.
+          leading: Navigator.of(context).canPop()
+              ? const BackButton(color: Colors.black)
+              : null,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.account_box,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // TODO: navigate to the profile page here
+              },
+            )
+          ],
+        ),
+        body: body);
+  }
+}
