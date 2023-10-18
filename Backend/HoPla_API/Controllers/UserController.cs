d2245c5de898a7f59ead9e34cf293892c667a7e1
timmaasgeesteranus <tim__mg@hotmail.com>
2023-10-18 17:54:34 +0200

    create backend template

---
+++ Backend/HoPla_API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using HoPla_API.Context;
+using HoPla_API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HoPla_API.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UserController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("getFirst")]
+        public async Task<IActionResult> GetTest()
+        {
+
+            try
+            {
+                User user = _appDbContext.Users.First();
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return NotFound("No user found");
+            }
+
+
+        }
+    }
+}
---
+++ Backend/HoPla_API/Entities/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HoPla_API.Entities
+{
+    public class Reservation
+    {
+        //[Required]
+        //public User User { get; set; }
+
+        //[Required]
+        //public Item Item { get; set; }
+
+        [Key]
+        public int TestId {  get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+    }
+}
