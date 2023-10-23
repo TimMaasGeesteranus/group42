b9c27af4d8ccbc4c650fe69cb1ebea889186e475
Clemens <mail@clemens.jann.xyz>
2023-10-23 10:17:22 +0200

    Feat: change to stateful widget

--- Frontend/lib/views/schedule.dart
+++ Frontend/lib/views/schedule.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ScheduleWidget extends StatelessWidget {
+class ScheduleWidget extends StatefulWidget {
   const ScheduleWidget({super.key});
 
+  @override
+  State<ScheduleWidget> createState() => _ScheduleWidgetState();
+}
+
+class _ScheduleWidgetState extends State<ScheduleWidget> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
