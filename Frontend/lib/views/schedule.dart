b8c3305fe7f827b8a2d28928fa013c3073bde970
Clemens <mail@clemens.jann.xyz>
2023-10-24 13:43:03 +0200

    Feat: add basic weekly overview

--- Frontend/lib/views/schedule.dart
+++ Frontend/lib/views/schedule.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+import '../util/ho_pla_scaffold.dart';
 
 class ScheduleWidget extends StatefulWidget {
   const ScheduleWidget({super.key});
@@ -8,8 +11,35 @@ class ScheduleWidget extends StatefulWidget {
 }
 
 class _ScheduleWidgetState extends State<ScheduleWidget> {
+  ReservationsDataSource source = ReservationsDataSource();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return HoPlaScaffold(
+        "Overview",
+        SfCalendar(
+          view: CalendarView.week,
+          onTap: _onTap,
+          dataSource: source,
+        ));
+  }
+
+  void _onTap(CalendarTapDetails details) {
+    // TODO: send request, error handling
+    var app = Appointment(
+      startTime: details.date!,
+      endTime: details.date!.add(const Duration(minutes: 60)),
+      subject: 'Reservation',
+      color: Colors.blue,
+    );
+
+    source.appointments?.add(app);
+    source.notifyListeners(CalendarDataSourceAction.add, [app]);
+  }
+}
+
+class ReservationsDataSource extends CalendarDataSource {
+  _ReservationsDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
