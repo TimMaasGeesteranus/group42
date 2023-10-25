110994b86ff6bf8545a80b93ae5a52febd6c4c9b
Clemens <mail@clemens.jann.xyz>
2023-10-25 09:58:31 +0200

    Feat: add FutureBuilder to weekly overview

--- Frontend/lib/views/schedule.dart
+++ Frontend/lib/views/schedule.dart
@@ -13,14 +13,35 @@ class ScheduleWidget extends StatefulWidget {
 class _ScheduleWidgetState extends State<ScheduleWidget> {
   ReservationsDataSource source = ReservationsDataSource();
 
+  /// This future will complete with the reservations fetched from the backend.
+  late Future<List<Appointment>> fetchReservationsFuture;
+
   @override
   Widget build(BuildContext context) {
     return HoPlaScaffold(
         "Overview",
-        SfCalendar(
-          view: CalendarView.week,
-          onTap: _onTap,
-          dataSource: source,
+        FutureBuilder<List<Appointment>>(
+          future: fetchReservationsFuture,
+          builder: (context, snap) {
+            if (!snap.hasData && !snap.hasError) {
+              // No data and no error => the request is still running
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snap.hasError) {
+              return Center(
+                child: Text(snap.error.toString()),
+              );
+            } else {
+              // Data is now definitely present.
+              source = ReservationsDataSource.withSource(snap.data ?? []);
+              return SfCalendar(
+                view: CalendarView.week,
+                onTap: _onTap,
+                dataSource: source,
+              );
+            }
+          },
         ));
   }
 
@@ -36,10 +57,24 @@ class _ScheduleWidgetState extends State<ScheduleWidget> {
     source.appointments?.add(app);
     source.notifyListeners(CalendarDataSourceAction.add, [app]);
   }
+
+  @override
+  void initState() {
+    fetchReservationsFuture = fetchReservations();
+  }
+
+  /// Async method that calls the backend
+  Future<List<Appointment>> fetchReservations() async {
+    // TODO: call backend here
+    return [];
+  }
 }
 
+/// Helper class needed for the plugin to function
 class ReservationsDataSource extends CalendarDataSource {
-  _ReservationsDataSource(List<Appointment> source) {
+  ReservationsDataSource();
+
+  ReservationsDataSource.withSource(List<Appointment> source) {
     appointments = source;
   }
 }
