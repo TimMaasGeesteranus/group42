e842b965a187a9994deac2803ea649d8b3080765
Clemens <mail@clemens.jann.xyz>
2023-11-03 13:24:02 +0100

    47 profile page in flutter (#61)
    * Ref: rename profile to settings
    
    * Feat: add profile widget

--- Frontend/lib/views/profile.dart
+++ Frontend/lib/views/profile.dart
@@ -1,6 +1,6 @@
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:ho_pla/util/ho_pla_scaffold.dart';
+import 'package:ho_pla/views/settings.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileWidget extends StatefulWidget {
@@ -11,53 +11,101 @@ class ProfileWidget extends StatefulWidget {
 }
 
 class _ProfileWidgetState extends State<ProfileWidget> {
-  bool darkMode = true;
+  final TextEditingController _nameTextController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
+  late final SharedPreferences preferences;
 
   @override
   Widget build(BuildContext context) {
-    return ThemeSwitchingArea(child: Builder(
-      builder: (context) {
-        return HoPlaScaffold(
-            "Profile",
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
-              child: Column(
-                children: [
-                  const Icon(
-                    Icons.person,
-                    size: 100,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30, bottom: 10),
-                    child: Container(
-                      color: Theme.of(context).cardColor,
-                      child: ThemeSwitcher(builder: (context) {
-                        return CheckboxListTile(
-                          value: darkMode,
-                          onChanged: switchTheme,
-                          title: const Text("Dark mode"),
-                        );
-                      }),
-                    ),
-                  )
-                ],
+    return HoPlaScaffold(
+        "Profile",
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: ListView(
+            children: [
+              const Icon(
+                Icons.person,
+                size: 100,
               ),
-            ));
-      },
-    ));
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Theme.of(context)
+                        .cardColor, // Set the background color here
+                    hintText: 'Name',
+                  ),
+                  controller: _nameTextController,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Theme.of(context)
+                        .cardColor, // Set the background color here
+                    hintText: 'Email',
+                  ),
+                  controller: _emailTextController,
+                ),
+              ),
+              Container(
+                color: Theme.of(context).cardColor,
+                width: double.infinity,
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                child: TextButton(
+                    onPressed: onClickSettings, child: const Text("Settings")),
+              ),
+              Container(
+                color: Theme.of(context).cardColor,
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextButton(
+                    onPressed: onClickPremium,
+                    child: const Text("Go Premium!")),
+              ),
+              //const Spacer(),
+              Container(
+                color: Theme.of(context).cardColor,
+                width: double.infinity,
+                margin: const EdgeInsets.only(
+                    bottom: 20, left: 30, right: 30, top: 60),
+                child: TextButton(
+                    onPressed: onClickSave, child: const Text("Save")),
+              ),
+            ],
+          ),
+        ));
   }
 
-  void switchTheme(bool? newValue) {
-    setState(() {
-      darkMode = newValue ?? darkMode;
-    });
-    save();
-    ThemeSwitcher.of(context)
-        .changeTheme(theme: darkMode ? ThemeData.dark() : ThemeData.light());
+  void onClickSettings() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return const SettingsWidget();
+    }));
   }
 
-  void save() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('darkmode', darkMode);
+  void onClickPremium() {
+    // TODO: premium
+  }
+
+  void onClickSave() {
+    // TODO: update backend
+    preferences.setString("name", _nameTextController.text);
+    preferences.setString("email", _emailTextController.text);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadPreferences();
+  }
+
+  void loadPreferences() async {
+    preferences = await SharedPreferences.getInstance();
+    _nameTextController.text = preferences.getString("name") ?? "";
+    _emailTextController.text = preferences.getString("email") ?? "";
   }
 }
---
+++ Frontend/lib/views/settings.dart
@@ -0,0 +1,63 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:flutter/material.dart';
+import 'package:ho_pla/util/ho_pla_scaffold.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsWidget extends StatefulWidget {
+  const SettingsWidget({super.key});
+
+  @override
+  State<SettingsWidget> createState() => _SettingsWidgetState();
+}
+
+class _SettingsWidgetState extends State<SettingsWidget> {
+  bool darkMode = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return ThemeSwitchingArea(child: Builder(
+      builder: (context) {
+        return HoPlaScaffold(
+            "Settings",
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 50),
+              child: Column(
+                children: [
+                  const Icon(
+                    Icons.person,
+                    size: 100,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30, bottom: 10),
+                    child: Container(
+                      color: Theme.of(context).cardColor,
+                      child: ThemeSwitcher(builder: (context) {
+                        return CheckboxListTile(
+                          value: darkMode,
+                          onChanged: switchTheme,
+                          title: const Text("Dark mode"),
+                        );
+                      }),
+                    ),
+                  )
+                ],
+              ),
+            ));
+      },
+    ));
+  }
+
+  void switchTheme(bool? newValue) {
+    setState(() {
+      darkMode = newValue ?? darkMode;
+    });
+    save();
+    ThemeSwitcher.of(context)
+        .changeTheme(theme: darkMode ? ThemeData.dark() : ThemeData.light());
+  }
+
+  void save() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('darkmode', darkMode);
+  }
+}
