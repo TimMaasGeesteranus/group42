3c671b89ce88917c5f17114a758c836fc4beac99
Clemens <mail@clemens.jann.xyz>
2023-11-06 17:57:49 +0100

    Feat: add devices overview

--- Frontend/lib/views/devices_overview.dart
+++ Frontend/lib/views/devices_overview.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:ho_pla/model/house.dart';
 import 'package:ho_pla/util/device_preview.dart';
 import 'package:ho_pla/util/ho_pla_scaffold.dart';
+import 'package:ho_pla/views/add_device.dart';
 
 class DevicesOverviewWidget extends StatefulWidget {
-  const DevicesOverviewWidget({super.key});
+  final House house;
+
+  const DevicesOverviewWidget(this.house, {super.key});
 
   @override
   State<DevicesOverviewWidget> createState() => _DevicesOverviewWidgetState();
@@ -18,29 +22,33 @@ class _DevicesOverviewWidgetState extends State<DevicesOverviewWidget> {
           children: [
             Row(
               children: [
-                Text("Student House #1"),
-                Spacer(),
+                Text(widget.house.name),
+                const Spacer(),
                 IconButton(
                     onPressed: onInformationButtonClicked,
                     icon: const Icon(Icons.info_outline))
               ],
             ),
             Expanded(
-              child: GridView(
+              child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
                   mainAxisSpacing: 10,
                   crossAxisSpacing: 10,
                   //childAspectRatio: 0.8
                 ),
-                children: [
-                  DevicePreviewWidget(
-                      AssetImage("assets/icons/washing-machine.png"),
-                      "Washing machine #1"),
-                  DevicePreviewWidget(
-                      AssetImage("assets/icons/washing-machine.png"),
-                      "Washing machine #1")
-                ],
+                itemCount: (widget.house.items.length ?? 0) + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  if (index < widget.house.items.length) {
+                    // TODO: get actual image here
+                    return DevicePreviewWidget(
+                        const AssetImage("assets/icons/washing-machine.png"),
+                        widget.house.items[index].name);
+                  } else {
+                    // Create card that allows the user to create a new device here
+                    return AddDeviceWidget(widget.house);
+                  }
+                },
               ),
             )
           ],
