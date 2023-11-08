302f0b5f43d33b4c1c75875c6921bebb56def922
timmaasgeesteranus <tim__mg@hotmail.com>
2023-11-08 20:22:18 +0100

    added styling

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ThemeProvider(
-        initTheme: darkMode ? ThemeData.dark() : ThemeData.light(),
+        initTheme: darkMode ? customDarkTheme : ThemeData.light(),
         builder: (context, myTheme) {
           return MaterialApp(
             title: 'HoPla Demo',
@@ -31,3 +31,34 @@ class MyApp extends StatelessWidget {
         });
   }
 }
+
+Color red = const Color(0xFFFF3636);
+Color orange = const Color(0xFFF79256);
+Color black = const Color(0xFF1E2023);
+Color blue = const Color(0xFF75F5EE);
+Color green = const Color(0xFF5DE877);
+Color grey = const Color(0xFF59575A);
+Color darkGrey = const Color(0xFF272D33);
+Color white = const Color(0xFFFFFFFF);
+
+final customDarkTheme = ThemeData.dark().copyWith(
+  primaryColor: red,
+  checkboxTheme: CheckboxThemeData(
+    fillColor: MaterialStateProperty.resolveWith((states) => states.contains(MaterialState.selected) ? red : grey)
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(red),
+      foregroundColor: MaterialStateProperty.all(white), // Set the text color to white for text buttons
+    ),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: red),
+    ),
+  ),
+  textSelectionTheme: TextSelectionThemeData(
+    selectionHandleColor: red,
+    cursorColor: white,
+  )
+);
