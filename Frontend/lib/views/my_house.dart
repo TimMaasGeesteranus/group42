f60589f3513dfbfa741b16d29ef0627983471691
Sven <svenvdp@hotmail.nl>
2023-11-29 14:32:14 +0100

    Scaffold with save button added

--- Frontend/lib/views/my_house.dart
+++ Frontend/lib/views/my_house.dart
@@ -14,53 +14,68 @@ class _MyHouseWidgetState extends State<MyHouseWidget> {
   TextEditingController nameController = TextEditingController();
 
   final List<String> usernames = <String>['A', 'B', 'C']; //Dummy usernames
+  static const double SIZEDBOXHEIGHT = 40;
 
   @override
   Widget build(BuildContext context) {
     return HoPlaScaffold(
         "Your House",
         Container(
-            padding: const EdgeInsets.all(8),
-            color: Theme.of(context).cardColor,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  //controller: nameController,
-                  initialValue: "My Kingdom",
-                  decoration: const InputDecoration(
-                    labelText: "House Name",
-                    border: OutlineInputBorder(),
+          padding: const EdgeInsets.all(8),
+          color: Theme.of(context).cardColor,
+          child: Scaffold(
+            body: Center(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                  TextFormField(
+                    //controller: nameController,
+                    initialValue: "My Kingdom",
+                    decoration: const InputDecoration(
+                      labelText: "House Name",
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const Text("House number"),
-                const Divider(),
-                const Text("#\t" "420"), //TODO insert houseid here
-                Text("${usernames.length} members"),
-                const Divider(),
-                ListView.separated(
-                  padding: const EdgeInsets.all(8),
-                  shrinkWrap: true,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      height: 50,
-                      child: Center(child: Text(usernames[index])),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      const Divider(),
-                  itemCount: usernames.length,
-                ),
-                ElevatedButton(
-                  onPressed: OnLeaveClicked,
-                  child: const Text('Leave House'),
-                )
-              ],
-            )));
+                  const SizedBox(height: SIZEDBOXHEIGHT,),
+                  const Text("House number"),
+                  const Divider(),
+                  const Text("#\t" "420"), //TODO insert houseid here
+                  const SizedBox(height: SIZEDBOXHEIGHT,),
+                  Text("${usernames.length} members"),
+                  const Divider(),
+                  ListView.separated(
+                    padding: const EdgeInsets.all(8),
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 50,
+                        child: Center(child: Text(usernames[index])),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                        const Divider(),
+                    itemCount: usernames.length,
+                  ),
+                  ElevatedButton(
+                    onPressed: OnLeaveClicked,
+                    child: const Text('Leave House'),
+                  )
+                ])),
+            floatingActionButton: FloatingActionButton(
+              onPressed: OnSaveClicked,
+              tooltip: 'Save changes',
+              child: const Text("Save"),
+            ),
+          ),
+        ));
   }
 
   OnLeaveClicked() {
+    return;
+  }
+
+  OnSaveClicked() {
     return;
   }
 }
