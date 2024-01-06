a06d1695bb4da2c281a8c60974dca07b171f96aa
Clemens <mail@clemens.jann.xyz>
2024-01-06 13:38:47 +0100

    143 improve design (#147)
    * Fix: clarify text box
    
    * Fix: image picker bug
    
    * Feat: add more icons
    
    * Feat: add more icons

--- Frontend/lib/util/custom_image_picker.dart
+++ Frontend/lib/util/custom_image_picker.dart
@@ -16,35 +16,35 @@ class CustomImagePickerState extends State<CustomImagePicker> {
   Widget build(BuildContext context) {
     return HoPlaScaffold(
       "Select an image",
-      Column(
-        children: <Widget>[
-          const SizedBox(height: 8),
-          Container(
-            height: 100,
-            child: ListView.builder(
-              itemCount: widget.imagePaths.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context, widget.imagePaths[index]);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(widget.imagePaths[index]),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                );
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: widget.imagePaths.length,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.pop(context, widget.imagePaths[index]);
               },
-            ),
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(widget.imagePaths[index]),
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+            );
+          },
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 110.0,
+            mainAxisExtent: 110, // Maximum width of each item
+            crossAxisSpacing: 8.0, // Space between columns
+            mainAxisSpacing: 8.0, // Space between rows
           ),
-        ],
+        ),
       ),
     );
   }
